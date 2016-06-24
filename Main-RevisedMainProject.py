1eb2a59715c949eac5431a7b5c7a63aa1780c7e5
happykju1218 <happykju1218@naver.com>
2016-06-25 05:37:02 +0900

    Update Main-RevisedMainProject.py
    complete

--- Main-RevisedMainProject.py
+++ Main-RevisedMainProject.py
@@ -1,8 +1,9 @@
-
 global UserSet
 global FriendSet
 global TweetSet
 global SelectedUser
+global dijkArr
+dijkArr = []
 UserSet = []
 FriendSet = []
 TweetSet = []
@@ -227,6 +228,241 @@ def DFS_main():
     graph.dfs()
     for i in range(0,len(vertices)):
         graph.print_vertex(i)
+
+"""--------------dijectra algorithm----------------------"""
+
+import sys
+INFTY = 1E10
+
+class Heap:
+    def __init__(self):
+        self.nelem = 0
+        self.A = []
+    def parent(self,n):
+        return (n-1)//2
+    def left(self,n):
+        return 2*n+1
+    def right(self,n):
+        return 2*n+2
+    def compare(self,a,b):
+        return a - b > 0
+    def exchange(self,i,j):
+        A = self.A
+        A[i],A[j] = A[j],A[i]
+    def heapify(self,i):
+        A = self.A
+        l = self.left(i)
+        r = self.right(i)
+        if l < self.nelem and self.compare(A[l], A[i]):
+            largest = l
+        else:
+            largest = i
+        if r < self.nelem and self.compare(A[r], A[largest]):
+            largest = r
+        if largest != i:
+            self.exchange(i,largest)
+            self.heapify(largest)
+            
+class PrioNode:
+    def __init__(self, key, n):
+        self.ndx = 0
+        self.n = n
+        self.key = key
+    def __repr__(self):
+        return "(%d:%d,%d)" % (self.ndx,self.n, self.key)
+
+class MaxQueue(Heap):
+    def __init__(self):
+        super().__init__()
+    def compare(self,a,b):
+        return a.key > b.key
+    def exchange(self,i,j):
+        A = self.A
+        A[i].ndx = j
+        A[j].ndx = i
+        super().exchange(i,j)
+    def update_key(self,i):
+        parent = lambda x: self.parent(i)
+        compare = lambda a,b: self.compare(a,b)
+        A = self.A
+        while i > 0 and not compare(A[parent(i)], A[i]):
+            self.exchange(i,parent(i))
+            i = parent(i)
+    def increase_key(self,i,key):
+        A = self.A
+        if key < A[i].key:
+            print ("Error")
+            sys.exit(-1)
+        A[i].key = key
+        self.update_key(i)
+    def insert(self,n):
+        A = self.A
+        while (len(A) < self.nelem):
+            A.append(None)
+        i = self.nelem
+        A.append(None)
+        self.nelem = self.nelem + 1
+        A[i] = n
+        A[i].ndx = i
+        self.update_key(i)
+    def extract(self):
+        elem = self.A[0]
+        self.exchange(0,self.nelem-1)
+        self.nelem = self.nelem - 1
+        self.heapify(0)
+        return elem
+    def is_empty(self):
+        return self.nelem == 0
+
+class MinQueue(MaxQueue):
+    def __init__(self):
+        super().__init__()
+    def compare(self,a,b):
+        return a.key < b.key
+    def update_key(self,i):
+        parent = lambda x: self.parent(i)
+        A = self.A
+        while i > 0 and not self.compare(A[parent(i)], A[i]):
+            self.exchange(i,parent(i))
+            i = parent(i)
+    def decrease_key(self,i,key):
+        A = self.A
+        if key > A[i].key:
+            print ("Error")
+            sys.exit(-1)
+        A[i].key = key
+        self.update_key(i)
+    def __repr__(self):
+        return "%a %a" % (self.nelem,self.A)
+
+class Adj:
+    def __init__(self, n):
+        self.n = n
+        self.next = None
+
+class Weight(Adj):
+    def __init__(self, n, w):
+        super().__init__(n)
+        self.w = w
+
+class Vertex:
+    def __init__(self, name):
+        self.parent = -1
+        self.name = name
+        self.n = 0
+        self.first = None
+    def add(self, v):
+        a = Adj()
+        a.n = v.n
+        a.next = self.first
+        self.first = a
+    def copy(self, other):
+        self.parent = other.parent
+        self.name = other.name
+        self.n = other.n
+        self.first = other.first
+
+class DijkVertex(Vertex):
+    def __init__(self, name):
+        super().__init__(name)
+        self.d = INFTY
+        self.priority = None
+    def __repr__(self):
+        return "(%a %a %a)" % (self.name,self.n,self.d)
+    def add(self, v, w):
+        a = Weight(v, w)
+        a.next = self.first
+        self.first = a
+    def set_priority(self,n):
+        self.priority = n
+    def decrease_key(self, q):
+        prio = self.priority
+        ndx = prio.ndx
+        q.decrease_key(ndx, self.d)
+        
+
+class Dijkstra:
+    def __init__(self):
+        self.vertices = []
+        self.q = MinQueue()
+    def add_vertex(self,name):
+        n = len(self.vertices)
+        v = DijkVertex(name)
+        v.n = n
+        self.vertices.append(v)
+        return v
+    def get_vertex(self,name):
+        for v in self.vertices:
+            if v.name == name:
+                return v
+        return None        
+    def print_vertex(self,n):
+        print (self.vertices[n].name, end=' ')
+        print (self.vertices[n].parent, end=' ')
+        print (self.vertices[n].d, end=' ')
+        p = self.vertices[n].first
+        while p:
+            print (p.n.name, end = ' ')
+            print (p.w, end = ' ')
+            p = p.next
+        print('')
+    def print_vertices(self):
+        for i in range(len(self.vertices)):
+            self.print_vertex(i)
+    def relax(self, u):
+        vset = self.vertices
+        q = self.q
+        p = u.first
+        while p:
+            v = p.n;
+            d = u.d + p.w
+            if d < v.d:
+                v.d = d
+                v.parent = u.n
+                dijkArr.append([v.name,v.d])
+                v.decrease_key(q)
+            p = p.next
+    def shortest_path(self):
+        q = self.q
+        vset = self.vertices
+        for v in vset:
+            n = PrioNode(v.d, v.n)
+            v.set_priority(n)
+            q.insert(n)
+        while not q.is_empty():
+            u = q.extract()
+            self.relax(vset[u.n])
+
+def Dijk_Main():
+    Dijk = Dijkstra()
+    global UserSet
+    global FriendSet
+    vertices = []
+    for i in range(len(UserSet)):
+        vertices.append(Dijk.add_vertex(str(UserSet[i][1])))
+
+    for i in range(len(vertices)):
+        friendList = getFriend(UserSet[i][0])
+        for t in range(len(friendList)):
+            f = binary_search(UserSet,friendList[t],0)
+            if (f != None):
+                vertices[i].add(vertices[f],len(friendList))
+    ruser = input ('user name (from) : ')
+    ruserIndex = -1
+    for i in range(len(UserSet)):
+        if(UserSet[i][1] == ruser):
+            ruserIndex  = i
+    if(ruserIndex == -1):
+        return print('no user')
+    vertices[ruserIndex].d = 0
+    Dijk.shortest_path()
+    heapsort(dijkArr,1)
+    print(dijkArr[0])
+    print(dijkArr[1])
+    print(dijkArr[2])
+    print(dijkArr[3])
+    print(dijkArr[4])
+    
 
 """------------------read data------------------"""
 def readUserProfile():
@@ -567,7 +803,7 @@ def Menu8():
     DFS_main()
 
 def Menu9():
-    return False
+    Dijk_Main()
 
 def Controller():
       print('')
