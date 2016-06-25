9519d8a495e4ca25b5605c71c0ecbac7b343ed9f
happykju1218 <happykju1218@naver.com>
2016-06-26 01:07:14 +0900

    Update Main-RevisedMainProject.py

--- Main-RevisedMainProject.py
+++ Main-RevisedMainProject.py
@@ -719,7 +719,7 @@ def UserInterFace():
 def MenuList():
       print('0. Read data files')
       print('1. Display statistics')
-      print('2. Top 5 most tweeted users')
+      print('2. Top 5 most tweeted words')
       print('3. Top 5 most tweeted users')
       print('4. Find users who tweeted a word(e.g, yeounsaedae)')
       print('5. Find all people who are friends of the above users')
